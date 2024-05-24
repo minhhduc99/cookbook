c7acb4a527c453fc366da9a1068b82514c009cf7
Minh Duc <81224867+minhhduc99@users.noreply.github.com>
2024-05-24 17:30:19 +0700

    Update docker-entrypoint.sh

--- docker/be/docker-entrypoint.sh
+++ docker/be/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-python3.8 -c 'import run; db.create_all()'
+python3.8 -c 'from backend import db; db.create_all()'
 
 # Execute the Flask application
-exec flask run
+exec flask run --host=0.0.0.0
