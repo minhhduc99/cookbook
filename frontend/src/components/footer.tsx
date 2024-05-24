d35a7ba48e4d08356a6a4a4218aa937fdcc8e16f
Minh Duc <81224867+minhhduc99@users.noreply.github.com>
2024-05-24 20:58:12 +0700

    Update footer.tsx

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,15 +1,6 @@
 export default function Footer() {
     return (
         <footer>
-            Made with &#10084;&#65039; by{' '}
-            <a
-                target='_blank'
-                rel='noreferrer'
-                href='https://www.github.com/The5thAxiom'
-            >
-                Samridh
-            </a>{' '}
-            <br />
             <a
                 target='_blank'
                 rel='noreferrer'
