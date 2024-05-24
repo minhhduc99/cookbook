8aba5495895f85716d71c9d9f459f2d7f307aa1a
Minh Duc <81224867+minhhduc99@users.noreply.github.com>
2024-05-24 20:59:22 +0700

    Update home.tsx

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -5,19 +5,6 @@ export default function Home() {
         <>
             <main>
                 <h1>Welcome!</h1>
-                <p>
-                    I am samridh (
-                    <NavLink end to='/user/@the5thaxiom'>
-                        @the5thaxiom
-                    </NavLink>
-                    ) and I created this site to show my own recipes and
-                    hopefully to see your favourite recipes.
-                </p>
-                <p>
-                    The website is still in development. So any user
-                    accounts/recipes you save might get deleted. Check out the
-                    site in a while to see if it is live (you'll know)
-                </p>
             </main>
         </>
     );
